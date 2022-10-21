1d15c6d90dc0cd31abcc00e3d6a415af714fdeb0
PauloSousa <paulo_sousa@trimble.com>
2022-10-21 06:48:22 -0300

    Cria modal editar e melhora busca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
 import { ConvertDataPipePipe } from './pipe/convert-data-pipe.pipe';
 import { InfotempFormatDataPipe } from './pipe/infotemp-format-data.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ConvertDataPipePipe, InfotempFormatDataPipe],
+  declarations: [
+    AppComponent,
+    EditDialogComponent,
+    ConvertDataPipePipe,
+    InfotempFormatDataPipe,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,7 +31,10 @@ import { InfotempFormatDataPipe } from './pipe/infotemp-format-data.pipe';
     FormsModule,
     MatProgressSpinnerModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
